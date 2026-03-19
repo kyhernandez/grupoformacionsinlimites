baa486c5336eff69859ec805ffc10d2e564d1ddb
kyhernandez <karenh.sistemas@gmail.com>
2026-03-18 22:06:05 -0500

    Se reakuza diseño responsivo para pantallas hasta 480px de la pagina principal

--- js/slider.js
+++ js/slider.js
@@ -40,3 +40,17 @@ const sliderAutomatico = () => {
     });
 }
 sliderAutomatico();
+
+// --- MENÚ HAMBURGUESA --- 
+
+const menu = document.querySelector("#menu");
+const abrir = document.querySelector("#abrir");
+const cerrar = document.querySelector("#cerrar");
+
+abrir.addEventListener("click", () => {
+    menu.classList.add("visible");
+})
+
+cerrar.addEventListener("click", () => {
+    menu.classList.remove("visible");
+})
